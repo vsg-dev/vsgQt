64a57c54147dc3d86bd70fda2f8a88c5a0fe12d6
Robert Osfield <robert@openscenegraph.com>
2021-09-10 11:37:31 +0100

    Initial cut of testbed using standard vsg windowing support for viewer

---
+++ src/vsgQt/main.cpp
@@ -0,0 +1,127 @@
+#include <vsg/all.h>
+#include <vsgXchange/all.h>
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    vsg::CommandLine arguments(&argc, argv);
+
+    // set up vsg::Options to pass in filepaths and ReaderWriter's and other IO realted options to use when reading and writing files.
+    auto options = vsg::Options::create();
+    options->fileCache = vsg::getEnv("VSG_FILE_CACHE");
+    options->paths = vsg::getEnvPaths("VSG_FILE_PATH");
+
+    // add vsgXchange's support for reading and writing 3rd party file formats
+    options->add(vsgXchange::all::create());
+
+    arguments.read(options);
+
+    auto windowTraits = vsg::WindowTraits::create();
+    windowTraits->windowTitle = "vsgviewer";
+    windowTraits->debugLayer = arguments.read({"--debug", "-d"});
+    windowTraits->apiDumpLayer = arguments.read({"--api", "-a"});
+    if (arguments.read({"--fullscreen", "--fs"})) windowTraits->fullscreen = true;
+    if (arguments.read({"--window", "-w"}, windowTraits->width, windowTraits->height)) { windowTraits->fullscreen = false; }
+    auto numFrames = arguments.value(-1, "-f");
+    auto horizonMountainHeight = arguments.value(0.0, "--hmh");
+
+    auto useQt = arguments.read("--qt");
+    if (arguments.read("--vsg")) useQt = false;
+
+    if (arguments.errors()) return arguments.writeErrorMessages(std::cerr);
+
+    if (argc <= 1)
+    {
+        std::cout << "Please specify a 3d model or image file on the command line." << std::endl;
+        return 1;
+    }
+
+    vsg::Path filename = arguments[1];
+
+    auto vsg_scene = vsg::read_cast<vsg::Node>(filename, options);
+    if (!vsg_scene)
+    {
+        std::cout << "Failed to load a valid scenene graph, Please specify a 3d model or image file on the command line." << std::endl;
+        return 1;
+    }
+
+
+    if (useQt)
+    {
+        //MainWindow w;
+        //w.show();
+        return a.exec();
+    }
+    else
+    {
+        try
+        {
+            // create the viewer and assign window(s) to it
+            auto viewer = vsg::Viewer::create();
+            auto window = vsg::Window::create(windowTraits);
+            if (!window)
+            {
+                std::cout << "Could not create windows." << std::endl;
+                return 1;
+            }
+
+            viewer->addWindow(window);
+
+            // compute the bounds of the scene graph to help position camera
+            vsg::ComputeBounds computeBounds;
+            vsg_scene->accept(computeBounds);
+            vsg::dvec3 centre = (computeBounds.bounds.min + computeBounds.bounds.max) * 0.5;
+            double radius = vsg::length(computeBounds.bounds.max - computeBounds.bounds.min) * 0.6;
+            double nearFarRatio = 0.001;
+
+            // set up the camera
+            auto lookAt = vsg::LookAt::create(centre + vsg::dvec3(0.0, -radius * 3.5, 0.0), centre, vsg::dvec3(0.0, 0.0, 1.0));
+
+            vsg::ref_ptr<vsg::ProjectionMatrix> perspective;
+            vsg::ref_ptr<vsg::EllipsoidModel> ellipsoidModel(vsg_scene->getObject<vsg::EllipsoidModel>("EllipsoidModel"));
+            if (ellipsoidModel)
+            {
+                perspective = vsg::EllipsoidPerspective::create(lookAt, ellipsoidModel, 30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio, horizonMountainHeight);
+            }
+            else
+            {
+                perspective = vsg::Perspective::create(30.0, static_cast<double>(window->extent2D().width) / static_cast<double>(window->extent2D().height), nearFarRatio * radius, radius * 4.5);
+            }
+
+            auto camera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(window->extent2D()));
+
+            // add close handler to respond the close window button and pressing escape
+            viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+            viewer->addEventHandler(vsg::Trackball::create(camera, ellipsoidModel));
+
+            auto commandGraph = vsg::createCommandGraphForView(window, camera, vsg_scene);
+            viewer->assignRecordAndSubmitTaskAndPresentation({commandGraph});
+
+            viewer->compile();
+
+            // rendering main loop
+            while (viewer->advanceToNextFrame() && (numFrames < 0 || (numFrames--) > 0))
+            {
+                // pass any events into EventHandlers assigned to the Viewer
+                viewer->handleEvents();
+
+                viewer->update();
+
+                viewer->recordAndSubmit();
+
+                viewer->present();
+            }
+        }
+        catch (const vsg::Exception& ve)
+        {
+            for (int i = 0; i < argc; ++i) std::cerr << argv[i] << " ";
+            std::cerr << "\n[Exception] - " << ve.message << " result = " << ve.result << std::endl;
+            return 1;
+        }
+
+        // clean up done automatically thanks to ref_ptr<>
+        return 0;
+    }
+}
