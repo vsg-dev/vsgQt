f7f22be8e8bda9022e2285e2257f7fb6bc9bcde6
Robert Osfield <robert@openscenegraph.com>
2023-06-23 09:55:35 +0100

    Revert "Removed unused variable"
    This reverts commit 434573daf3ef43a0be325d9bc1245645379d0a5b.

--- include/vsgQt/ViewerWindow.h
+++ include/vsgQt/ViewerWindow.h
@@ -31,6 +31,7 @@ namespace vsgQt
         virtual ~ViewerWindow();
 
         vsg::ref_ptr<vsg::WindowTraits> traits;
+        vsg::ref_ptr<vsg::Instance> instance;
         vsg::ref_ptr<vsg::Viewer> viewer;
 
         vsg::ref_ptr<vsg::WindowAdapter> windowAdapter;
